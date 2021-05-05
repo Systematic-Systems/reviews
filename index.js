cab39a825918261eb032690e52085457bfa623fa
Caleb Iuliano <calebiuliano@gmail.com>
2021-05-05 19:55:02 -0400

    Finished metadata

--- index.js
+++ index.js
@@ -9,36 +9,102 @@ app.use(express.json());
 
 app.get('/reviews', (req, res) => {
   const id = req.query.product_id;
-  const reviewQuery = `SELECT * FROM reviews FULL OUTER JOIN photos ON photos.review_id = reviews.id WHERE product_id=${id}`;
+  const reviewQuery = `SELECT * FROM reviews FULL OUTER JOIN photos ON  reviews.id = photos.review_id WHERE product_id=${id} LIMIT 10`;
   db.query(reviewQuery, (err, results) => {
     if (err) {
       console.log(err);
     }
-    const review = results.rows[0];
-    const data = {
-      review_id: review.review_id,
-      product_id: review.product_id,
-      rating: review.rating,
-      date: review.date,
-      summary: review.summary,
-      body: review.body,
-      recommend: review.recommend,
-      reported: review.reported,
-      reviewer_name: review.reviewer_name,
-      reviewer_email: review.reviewer_email,
-      response: review.response,
-      helpfulness: review.helpfulness,
-      photos: [{
-        id: review.id,
-        url: review.url,
-      }],
-    };
+    const reviews = results.rows;
+    const data = reviews.map((review) => {
+      if (review.id !== null) {
+        const info = {
+          review_id: review.review_id,
+          product_id: review.product_id,
+          rating: review.rating,
+          date: review.date,
+          summary: review.summary,
+          body: review.body,
+          recommend: review.recommend,
+          reported: review.reported,
+          reviewer_name: review.reviewer_name,
+          reviewer_email: review.reviewer_email,
+          response: review.response,
+          helpfulness: review.helpfulness,
+          photos: [{
+            id: review.id,
+            url: review.url,
+          }],
+        };
+        return info;
+      }
+      return null;
+    }).filter((review) => review !== null);
+    // console.log(data);
     res.send(data);
   });
 });
 
-app.get('/meta', (req, res) => {
+app.get('/reviews/meta/', (req, res) => {
+  const id = req.query.product_id;
+  const query = `SELECT review_id, rating, recommend, characteristic_id, value FROM reviews FULL OUTER JOIN characteristics ON reviews.id = characteristics.review_id WHERE product_id=${id}`;
+  db.query(query, (err, results) => {
+    const metaResults = results.rows;
+    let reviews = {
+    };
+    let ratings = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+    };
+    let recommend = { true: 0, false: 0, };
+    let tracker = {
 
+    };
+    let characteristics = {
+
+    };
+    metaResults.forEach((result) => {
+      const reviewID = result.review_id;
+      if (reviews[reviewID] === undefined) {
+        reviews[reviewID] = true;
+        const currentRating = result.rating;
+        ratings[currentRating] += 1;
+        if (result.recommend) {
+          recommend.true += 1;
+        } else {
+          recommend.false += 1;
+        }
+      }
+      const characteristic = result.characteristic_id;
+      if (tracker[characteristic] === undefined) {
+        tracker[characteristic] = {
+          id: characteristic,
+          value: result.value,
+          count: 1,
+          ave: result.value,
+        };
+        characteristics[characteristic] = {
+          id: characteristic,
+          ave: result.value,
+        };
+      } else {
+        tracker[characteristic].count += 1;
+        tracker[characteristic].value += 1;
+        tracker[characteristic].ave = tracker[characteristic].value / tracker[characteristic].count;
+        characteristics[characteristic].ave = tracker[characteristic].ave;
+      }
+    });
+    const data = {
+      product_id: id,
+      ratings: ratings,
+      recommended: recommend,
+      characteristics: characteristics,
+    };
+    // console.log(data);
+    res.send(data);
+  });
 });
 
 app.post('/reviews', (req, res) => {
